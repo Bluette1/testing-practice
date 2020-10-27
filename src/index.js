e9d0886936e0788c8d5d278a520c929a2733bc58
Bluette1 <marylene.sawyer@gmail.com>
2020-10-27 13:04:18 +0200

    Add `reverseString` function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-const capitalize = (string) => {
-  const resultString = [];
-  string = string.split(' ');
-  string.forEach(word => {
-    const splitWord = word.split('');
-    splitWord[0] = splitWord[0].toUpperCase();
-    word = splitWord.join('');
-    resultString.push(word);
-  });
-  return resultString.join(' ');
-}
+ const capitalize = (string) => {
+   const resultString = [];
+   string = string.split(' ');
+   string.forEach(word => {
+     const splitWord = word.split('');
+     splitWord[0] = splitWord[0].toUpperCase();
+     word = splitWord.join('');
+     resultString.push(word);
+   });
+   return resultString.join(' ');
+ }
 
-module.exports = { capitalize }
+ const reverseString = (string) => {
+   let reversedString = '';
+   for (let i = string.length - 1; i >= 0; i -= 1) {
+     reversedString += string.charAt(i);
+   }
+   return reversedString;
+ }
+
+ export { capitalize, reverseString }
--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,12 @@
-import index from '../src/index'
+import { capitalize, reverseString } from '../src/index'
+// import { cube, foo, graph } from './my-module.js';
 
 test('capitalizes a given string', () => {
-  expect(index.capitalize('we')).toBe('We');
+  expect(capitalize('We')).toBe('We');
+  expect(capitalize('we')).toBe('We');
+});
+
+test('reverses a given string', () => {
+  expect(reverseString('ewe')).toBe('ewe');
+  expect(reverseString('load')).toBe('daol');
 });
