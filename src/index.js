2510a722c2917fe5bca5dd6d7e4488d294513336
descholar-ceo <emmanuellamugi@gmail.com>
2020-10-28 09:39:01 +0200

    Add cipher function

--- src/index.js
+++ src/index.js
@@ -22,4 +22,41 @@ const calculator = {
   multiply: (first, sec) => first * sec,
 };
 
+const encryptor = (range, encrypted) => {
+  const max = range.reduce((a, b) => Math.max(a, b));
+  const min = range.reduce((a, b) => Math.min(a, b));
+  if (encrypted > max) {
+    return String.fromCharCode(encrypted);
+  }
+
+  encrypted = (encrypted - max) + (min - 1);
+  return String.fromCharCode(encrypted);
+};
+
+const getRange = (start, end) => {
+  if (start === end) return [start];
+  return [start, ...getRange(start + 1, end)];
+};
+
+const ceasarCipher = (str, key) => {
+  const strArr = str.split('');
+  const strDigits = [];
+  for (let i = 0; i < strArr.length; i += 1) {
+    console.log(`current index is: ${i} and the char is: ${strArr[i]} and the code is: ${strArr[i].charCodeAt(i)}`);
+    if (getRange(65, 90).includes(strArr[i].charCodeAt(i))) {
+      const encrypted = (strArr[i].charCodeAt(i) + key) % 26;
+      strDigits.push(encryptor((getRange(65, 90)), encrypted));
+    } else if (getRange(97, 122).includes(strArr[i].charCodeAt(i))) {
+      // encrypted = strArr[i].charCodeAt(i) + key % 26
+      // strDigits.push(encryptor((getRange(97, 122)), encrypted))
+    } else {
+      strDigits.push(strArr[i]);
+    }
+  }
+  return strDigits.join('');
+};
+
+// console.log(getRange(65,90));
+console.log(ceasarCipher('people', 5));
+
 export { capitalize, reverseString, calculator };
